77e21f920370c771a9f67913d60d5b502a18210f
Spec <dawid.spec.jachowicz@gmail.com>
2021-01-25 21:45:28 +0100

    Add implementation of Attributes - Abstract Attribute - Strength, Dexterity and Intelligence - Corresponding unit tests

---
+++ src/Statistics/Implementations/PrimaryStatistics/Attributes/Dexterity.cs
@@ -0,0 +1,7 @@
+﻿namespace Ares.Statistics.Implementations.PrimaryStatistics.Attributes
+{
+    public class Dexterity : Attribute
+    {
+        public override string Name { get; } = "Dexterity";
+    }
+}
---
+++ src/Statistics/Implementations/PrimaryStatistics/Attributes/Strength.cs
@@ -0,0 +1,7 @@
+﻿namespace Ares.Statistics.Implementations.PrimaryStatistics.Attributes
+{
+    public class Strength : Attribute
+    {
+        public override string Name { get; } = "Strength";
+    }
+}
