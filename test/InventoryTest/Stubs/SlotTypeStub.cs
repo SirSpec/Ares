12ef378b8dabb135d0d30b4d61f94792df408448
Spec <dawidjachowicz91@gmail.com>
2021-01-31 15:19:28 +0100

    Adjust SlotTypeStub to include Flags

--- test/InventoryTest/Stubs/SlotTypeStub.cs
+++ test/InventoryTest/Stubs/SlotTypeStub.cs
@@ -1,12 +1,15 @@
+using System;
+
 namespace Ares.InventoryTest.Stubs
 {
+    [Flags]
     public enum SlotTypeStub
     {
-        Slot1,
-        Slot2,
-        Slot3,
-        Slot4,
-        Slot5,
-        Slot6,
+        Slot1 = 1,
+        Slot2 = 2,
+        Slot3 = 4,
+        Slot4 = 8,
+        Slot5 = 16,
+        Slot6 = 32,
     }
 }
