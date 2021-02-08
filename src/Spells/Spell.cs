5d7fa543425b1f7d5105ddb6220fcd940abeefbd
Spec <dawid.spec.jachowicz@gmail.com>
2021-02-08 07:48:42 +0100

    Add Spell

---
+++ src/Spells/Spell.cs
@@ -0,0 +1,14 @@
+﻿using Ares.Spells.Effects;
+
+namespace Ares.Spells
+{
+    public readonly struct Spell
+    {
+        public string Name { get; }
+        public IEffect Effect { get; }
+        public int Cost { get; }
+
+        public Spell(string name, IEffect effect, int cost) =>
+            (Name, Effect, Cost) = (name, effect, cost);
+    }
+}
