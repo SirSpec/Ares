1f0cde7818f488c1305641aacd31b26c30be397c
Spec <dawidjachowicz91@gmail.com>
2021-02-10 00:01:03 +0100

    Add Buff implementation

---
+++ src/GameSystem/Effects/Buff.cs
@@ -0,0 +1,13 @@
+﻿using Ares.Statistics;
+
+namespace Ares.GameSystem.Effects
+{
+    public readonly struct Buff : IBuff
+    {
+        public string Name { get; }
+        public IEnhancement<IStatistic> Enhancement { get; }
+
+        public Buff(string name, IEnhancement<IStatistic> enhancement) =>
+            (Name, Enhancement) = (name, enhancement);
+    }
+}
