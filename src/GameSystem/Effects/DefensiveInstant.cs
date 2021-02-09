a5f2a202e86d47094c757250d4451fbeafc9bc07
Spec <dawid.spec.jachowicz@gmail.com>
2021-02-10 00:01:54 +0100

    Add DefensiveInstant and OffensiveInstant implementation

---
+++ src/GameSystem/Effects/DefensiveInstant.cs
@@ -0,0 +1,11 @@
+﻿namespace Ares.GameSystem.Effects
+{
+    public readonly struct DefensiveInstant : IInstant<int>
+    {
+        public string Name { get; }
+        public int Value { get; }
+
+        public DefensiveInstant(string name, int value) =>
+            (Name, Value) = (name, value);
+    }
+}
