905fd5f7a8dac57bfc80135684f03af06b94ee5d
Spec <dawidjachowicz91@gmail.com>
2021-02-10 00:01:54 +0100

    Add DefensiveInstant and OffensiveInstant implementation

---
+++ src/GameSystem/Effects/OffensiveInstant.cs
@@ -0,0 +1,13 @@
+﻿using Ares.GameSystem.Weapons;
+
+namespace Ares.GameSystem.Effects
+{
+    public readonly struct OffensiveInstant : IInstant<DamageDealt>
+    {
+        public string Name { get; }
+        public DamageDealt Value { get; }
+
+        public OffensiveInstant(string name, DamageDealt damage) =>
+            (Name, Value) = (name, damage);
+    }
+}
