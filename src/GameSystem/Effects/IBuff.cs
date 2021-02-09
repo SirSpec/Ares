c3f029b68de0ee75099aa3322d1cb812815e5333
Spec <dawid.spec.jachowicz@gmail.com>
2021-02-09 23:25:03 +0100

    Add IBuff

---
+++ src/GameSystem/Effects/IBuff.cs
@@ -0,0 +1,10 @@
+﻿using Ares.Spells.Effects;
+using Ares.Statistics;
+
+namespace Ares.GameSystem.Effects
+{
+    public interface IBuff : IEffect
+    {
+        IEnhancement<IStatistic> Enhancement { get; }
+    }
+}
