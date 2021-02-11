4d65382377c1eb4869c6d7da87843dc8e027a7b9
Spec <dawid.spec.jachowicz@gmail.com>
2021-02-11 21:10:35 +0100

    Change Buffs implementations to reference type

--- src/GameSystem/Effects/OffensiveInstant.cs
+++ src/GameSystem/Effects/OffensiveInstant.cs
@@ -2,7 +2,7 @@
 
 namespace Ares.GameSystem.Effects
 {
-    public readonly struct OffensiveInstant : IInstant<DamageDealt>
+    public class OffensiveInstant : IInstant<DamageDealt>
     {
         public string Name { get; }
         public DamageDealt Value { get; }
