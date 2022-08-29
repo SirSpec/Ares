7bda2c408505f37ed5679ef25daafa1ebaeb21ba
Spec <dawid.spec.jachowicz@gmail.com>
2022-08-29 18:44:05 +0200

    Add DomainException

---
+++ src/Ares.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,11 @@
+namespace Ares.Domain.Exceptions;
+
+public class DomainException : Exception
+{
+    public DomainException(string code, params (string Key, object Value)[] parameters)
+        : base(code)
+    {
+        foreach (var parameter in parameters)
+            Data.Add(parameter.Key, parameter.Value);
+    }
+}
