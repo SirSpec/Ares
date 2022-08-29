6dbb2a9fc43ae87323cd89e455ce90d4e2fc5ed8
Spec <dawid.spec.jachowicz@gmail.com>
2022-08-29 18:49:14 +0200

    Add DieRoll and corresponding Unit tests

---
+++ src/Ares.Domain/ValueObjects/DieRoll.cs
@@ -0,0 +1,31 @@
+using Ares.Domain.Constants;
+using Ares.Domain.Exceptions;
+
+namespace Ares.Domain.ValueObjects;
+
+public record DieRoll
+{
+    public int Value { get; }
+    public int Type { get; }
+
+    public DieRoll(uint value, uint type)
+    {
+        Value = value > 0 && value <= type
+            ? (int)value
+            : throw new DomainException(ErrorCodes.DieRoll.InvalidValue, (nameof(value), value));
+
+        Type = (int)type;
+    }
+
+    public void Deconstruct(out int value, out int type)
+    {
+        value = Value;
+        type = Type;
+    }
+
+    public static implicit operator int(DieRoll dieRoll) =>
+        dieRoll.Value;
+
+    public override string ToString() =>
+        $"{Value}(d{Type})";
+}
---
+++ test/Ares.Domain.Test/ValueObjects/DieRollTest.cs
@@ -0,0 +1,53 @@
+using Ares.Domain.Exceptions;
+using Ares.Domain.ValueObjects;
+using Xunit;
+
+namespace Ares.Domain.Test.ValueObjects;
+
+public class DieRollTest
+{
+    [Theory]
+    [InlineData(0, 0)]
+    [InlineData(0, 1)]
+    [InlineData(1, 0)]
+    [InlineData(2, 1)]
+    public void DieRoll_InvalidValue_ThrowsDomainException(uint value, uint type)
+    {
+        var action = () => new DieRoll(value, type);
+
+        Assert.Throws<DomainException>(action);
+    }
+
+    [Theory]
+    [InlineData(1, 2)]
+    [InlineData(2, 2)]
+    public void ValueAndType_ValidData_ValidValueAndType(uint value, uint type)
+    {
+        var sut = new DieRoll(value, type);
+
+        Assert.Equal((int)value, sut.Value);
+        Assert.Equal((int)type, sut.Type);
+    }
+
+    [Fact]
+    public void Deconstruct_DieRoll_ValueAndType()
+    {
+        (int Value, int Type) expected = (2, 5);
+
+        (int value, int type) = new DieRoll(2, 5);
+
+        Assert.Equal(expected.Value, value);
+        Assert.Equal(expected.Type, type);
+    }
+
+    [Fact]
+    public void ImplicitOperatorInt_DieRoll_IntValue()
+    {
+        int expectedValue = 2;
+        var sut = new DieRoll(((uint)expectedValue), 5);
+
+        int value = sut;
+
+        Assert.Equal(expectedValue, value);
+    }
+}
