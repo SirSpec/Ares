8bf45af7e28e0d3797e8ceab03e925f606cc9e57
Spec <dawid.spec.jachowicz@gmail.com>
2022-08-29 18:46:57 +0200

    Add ErrorCodes

---
+++ src/Ares.Domain/Constants/ErrorCodes.cs
@@ -0,0 +1,33 @@
+namespace Ares.Domain.Constants;
+
+public static class ErrorCodes
+{
+    public static class DieRoll
+    {
+        public const string InvalidValue = $"{nameof(Ares.Domain.ValueObjects.DieRoll)}:{nameof(InvalidValue)}";
+        public const string InvalidType = $"{nameof(Ares.Domain.ValueObjects.DieRoll)}:{nameof(InvalidType)}";
+        public const string InvalidRollCount = $"{nameof(Ares.Domain.ValueObjects.DieRoll)}:{nameof(InvalidRollCount)}";
+    }
+
+    public static class AbilityScore
+    {
+        public const string InvalidValue = $"{nameof(Ares.Domain.ValueObjects.AbilityScore)}:{nameof(InvalidValue)}";
+    }
+
+    public static class Level
+    {
+        public const string InvalidValue = $"{nameof(Ares.Domain.ValueObjects.Level)}:{nameof(InvalidValue)}";
+    }
+
+    public static class Experience
+    {
+        public const string InvalidValue = $"{nameof(Ares.Domain.ValueObjects.Experience)}:{nameof(InvalidValue)}";
+    }
+
+    public static class Character
+    {
+        public const string MaximumExperienceGained = $"{nameof(Ares.Domain.Entities.Character)}:{nameof(MaximumExperienceGained)}";
+        public const string MaximumSuccessededDeathSavesReached = $"{nameof(Ares.Domain.Entities.Character)}:{nameof(MaximumSuccessededDeathSavesReached)}";
+        public const string MaximumFailedDeathSavesReached = $"{nameof(Ares.Domain.Entities.Character)}:{nameof(MaximumFailedDeathSavesReached)}";
+    }
+}
