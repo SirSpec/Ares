3e10e0e5890db166339f05ae53d5926cc3a176ce
Spec <dawid.spec.jachowicz@gmail.com>
2022-10-05 18:16:30 +0200

    Add Alignment enum

---
+++ src/Ares.Domain/Constants/Alignment.cs
@@ -0,0 +1,15 @@
+namespace Ares.Domain.Constants;
+
+public enum Alignment
+{
+    Unaligned,
+    LawfulGood,
+    NeutralGood,
+    ChaoticGood,
+    LawfulNeutral,
+    Neutral,
+    ChaoticNeutral,
+    LawfulEvil,
+    NeutralEvil,
+    ChaoticEvil,
+}
