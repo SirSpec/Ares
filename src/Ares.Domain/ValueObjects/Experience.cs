cc0cc218e601f9fe455e32123f352f6467efbd20
Spec <dawid.spec.jachowicz@gmail.com>
2022-10-06 12:14:12 +0200

    Add IsMaximum getter to Experience

--- src/Ares.Domain/ValueObjects/Experience.cs
+++ src/Ares.Domain/ValueObjects/Experience.cs
@@ -7,12 +7,15 @@ public record Experience
 {
     public const int Maximum = 355000;
     public const int Minimum = 0;
-    public int Value { get; }
 
     public Experience(int value) =>
         Value = value >= Minimum && value <= Maximum
             ? value
             : throw new DomainException(ErrorCodes.Experience.InvalidValue, (nameof(value), value));
+
+    public int Value { get; }
+    public bool IsMaximum =>
+        Value == Maximum;
 
     public Experience IncreasedBy(Experience experience) =>
         new Experience(Value + experience);
