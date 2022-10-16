b0f66edce1ccb8059f9b4c32827e1b52f45e559d
Spec <dawid.spec.jachowicz@gmail.com>
2022-10-16 07:05:25 +0200

    Add Condition enum

---
+++ src/Ares.Domain/Constants/Condition.cs
@@ -0,0 +1,22 @@
+namespace Ares.Domain.Constants;
+
+public enum Condition
+{
+    Stable,
+    Dead,
+    Blinded,
+    Charmed,
+    Deafened,
+    Frightened,
+    Grappled,
+    Incapacitated,
+    Invisible,
+    Paralyzed,
+    Petrified,
+    Poisoned,
+    Prone,
+    Restrained,
+    Stunned,
+    Unconscious,
+    Exhaustion
+}
