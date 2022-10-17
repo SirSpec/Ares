60fb2d784895f8f908b90819f0266f632dd3e0a3
Spec <dawid.spec.jachowicz@gmail.com>
2022-10-17 06:57:18 +0200

    Add Skill enum

---
+++ src/Ares.Domain/Constants/Skill.cs
@@ -0,0 +1,32 @@
+namespace Ares.Domain.Constants;
+
+public enum Skill
+{
+    // Strength
+    Athletics,
+
+    // Dexterity
+    Acrobatics,
+    SleightOfHand,
+    Stealth,
+
+    // Intelligence
+    Arcana,
+    History,
+    Investigation,
+    Nature,
+    Religion,
+
+    // Wisdom
+    AnimalHandling,
+    Insight,
+    Medicine,
+    Perception,
+    Survival,
+
+    // Charisma
+    Deception,
+    Intimidation,
+    Performance,
+    Persuasion
+}
